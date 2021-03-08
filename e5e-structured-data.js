7f23eac968b94de6399ad6a4dba95faa14630d16
bbaumgartner <bbaumgartner@anexia-it.com>
2021-03-08 11:58:32 +0100

    Modified function

--- e5e-structured-data.js
+++ e5e-structured-data.js
@@ -1,5 +1,5 @@
 exports.myfunction = (event, context) => {
-  console.log(`${event.data.a} + ${event.data.b}`);
+  console.log(`${event.data.a} + ${event.data.b}`, event.data.a + event.data.b);
   return {
     data: event.data.a + event.data.b,
   };
