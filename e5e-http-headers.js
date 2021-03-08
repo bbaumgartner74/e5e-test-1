28a1dcf66c4ce91801f9635377ce6792e8433915
bbaumgartner <bbaumgartner@anexia-it.com>
2021-03-08 12:11:12 +0100

    reset function

--- e5e-http-headers.js
+++ e5e-http-headers.js
@@ -1,5 +1,4 @@
 exports.myfunction = (event, context) => {
-  console.log(event["request_headers"]["content-type"] || "*/*");
   return {
     response_headers: {
       "x-sent-content-type": event["request_headers"]["content-type"] || "*/*",
