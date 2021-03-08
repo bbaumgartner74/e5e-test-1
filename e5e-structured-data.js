e005bf308b0c9d600ff0d8c86144a254fb94e015
bbaumgartner <bbaumgartner@anexia-it.com>
2021-03-08 12:01:15 +0100

    Reset function

--- e5e-structured-data.js
+++ e5e-structured-data.js
@@ -1,5 +1,4 @@
 exports.myfunction = (event, context) => {
-  console.log(`${event.data.a} + ${event.data.b}`, event.data.a + event.data.b);
   return {
     data: event.data.a + event.data.b,
   };
