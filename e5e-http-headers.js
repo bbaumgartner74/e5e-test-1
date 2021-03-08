13c68506fd64d4cc78538624a8e8ef854ff8292f
bbaumgartner <bbaumgartner@anexia-it.com>
2021-03-08 12:09:36 +0100

    modified function to add console.log

--- e5e-http-headers.js
+++ e5e-http-headers.js
@@ -1,4 +1,5 @@
 exports.myfunction = (event, context) => {
+  console.log(event["request_headers"]["content-type"] || "*/*");
   return {
     response_headers: {
       "x-sent-content-type": event["request_headers"]["content-type"] || "*/*",
